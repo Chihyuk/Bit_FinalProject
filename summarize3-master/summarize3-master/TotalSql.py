924f4ebdbd94ba05ed7bf39204472ff90023803d
song <chihyuk1223@gmail.com>
2022-01-11 14:09:02 +0900

    앞에서 넣기와 뒤에서 넣기 구분

--- summarize3-master/summarize3-master/TotalSql.py
+++ summarize3-master/summarize3-master/TotalSql.py
@@ -3,7 +3,7 @@
 from SqlCon import SqlCon
 
 class TotalSql():
-    # 언론사 전체 가져오기
+    # 기사 내용 전체 가져오기
     @staticmethod
     def findContent():
         cursor = SqlCon.Cursor()
@@ -15,18 +15,48 @@ class TotalSql():
         except:
             return None
         else:
-            #return row
             return row
 
+    # 기사 전체 내용 insert
+    @staticmethod
     def insertSum(nid, nsc):
         cursor = SqlCon.Cursor()
         query = str.format("insert into N_summarization (n_id, ns_content) values({0}, '{1}')", nid, nsc)
         try: 
             cursor.execute(query)
-            row = cursor.fetchall()     # 검색 결과 중에 하나의 Row를 fetch하시오.
-            #row = cursor.fetchone()
             SqlCon.Commit()
         except:
+            return None
+        else:
+            return True
+
+    # 기사 내용 앞에서 가져오기
+    @staticmethod
+    def findContent_front():
+        cursor = SqlCon.Cursor()
+        query = str.format("select n_id, n_content from N_content order by n_id desc")
+        try: 
+            cursor.execute(query)
+            row = cursor.fetchall()
+            SqlCon.Commit()
+        except:
+            return None
+        else:
+            return row
+
+    # 앞에서 가져온 기사 넣기
+    @staticmethod
+    def insertSum_front(nid, nsc):
+        cursor = SqlCon.Cursor()
+        query = str.format("insert into N_summarization (n_id, ns_content) values({0}, '{1}')", nid, nsc)
+        exist = 0
+        try: 
+            cursor.execute(query)
+            SqlCon.Commit()
+        except:
+            exist += 1
+            if exist >= 5:
+                return None
             return None
         else:
             return True
--- summarize3-master/summarize3-master/main.py
+++ summarize3-master/summarize3-master/main.py
@@ -2,15 +2,23 @@ from OktTokenizer import OktTokenizer
 from textrankr import TextRank
 from TotalSql import TotalSql
 
-def main():
+def main(a):
   mytokenizer: OktTokenizer = OktTokenizer()
   textrank: TextRank = TextRank(mytokenizer)
 
-  conts = TotalSql.findContent()
-  for cont in conts:
-    summarized: str = textrank.summarize(cont[1])
-    print(summarized)
-    TotalSql.insertSum(cont[0], summarized)
+  if a == 1:
+    conts = TotalSql.findContent()
+    for cont in conts:
+      summarized: str = textrank.summarize(cont[1])
+      print(cont[0])
+      TotalSql.insertSum(cont[0], summarized)
+      
+  elif a == 2:
+    conts = TotalSql.findContent_front()
+    for cont in conts:
+      summarized: str = textrank.summarize(cont[1])
+      print(cont[0])
+      TotalSql.insertSum_front(cont[0], summarized)
 
 if __name__ == "__main__":
-    main()
+    main(2)
